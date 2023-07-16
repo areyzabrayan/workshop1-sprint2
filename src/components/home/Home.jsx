4534cc59bf97ae8d4d0309d66546d8faddde2acc
Maria Paulina Parra <mariapaulinap0531@gmail.com>
2023-07-16 15:31:37 -0500

    footer

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../header/Header'
 import Destopk from '../destopk/destopk'
 import './home.scss'
+import Footer from '../footer/footer'
 
 function Home() {
   return (
     <>
     <Header />
     <Destopk />
+    <Footer />
 
     </>
   )
